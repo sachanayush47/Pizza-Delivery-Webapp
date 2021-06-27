0eb89f6811963051aaab7cc1dbf94d3644b54b52
Ayush Sachan <sachan.ayush47@gmail.com>
2021-06-27 13:09:06 +0530

    Home, login, registration and cart UI completed

--- server.js
+++ server.js
@@ -3,19 +3,32 @@ const expressLayouts = require("express-ejs-layouts");
 const ejs = require("ejs");
 const path = require("path");
 
-
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-// Set template engine.
-//app.use(expressLayouts);
+app.use(express.static("public"));
+
+// Set template engine and layouts.
+app.use(expressLayouts);
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "resources", "views"));
+app.set("views", path.join("resources", "views"));
 
 app.get("/", (req, res) => {
     res.render("home", {});
 });
 
+app.get("/cart", (req, res) => {
+    res.render("customers/cart", {});
+});
+
+app.get("/login", (req, res) => {
+    res.render("auth/login", {});
+});
+
+app.get("/register", (req, res) => {
+    res.render("auth/register", {});
+});
+
 app.listen(PORT, () => {
     console.log("Server running on port", PORT);
-})
+});
